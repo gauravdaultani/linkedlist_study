25248c23273e2cef0b40ecfdb75b617f0fca9da9
Gaurav <gauravdaultani8@gmail.com>
2024-01-01 23:57:22 +0530

    made some changes

--- insert_end_cll.c
+++ insert_end_cll.c
@@ -1,37 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure for a node
 struct Node {
     int data;
     struct Node* next;
 };
 
-// Function to insert a node at the end of a circular linked list
 void insertAtEnd(struct Node** head, int data) {
-    // Create a new node
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
     newNode->next = NULL;
 
-    // If the list is empty, make the new node the head
     if (*head == NULL) {
         *head = newNode;
         newNode->next = *head;
     } else {
-        // Traverse to the last node
         struct Node* temp = *head;
         while (temp->next != *head) {
             temp = temp->next;
         }
 
-        // Insert the new node at the end
         temp->next = newNode;
         newNode->next = *head;
     }
 }
 
-// Function to print the circular linked list
 void printList(struct Node* head) {
     if (head == NULL) {
         printf("List is empty.\n");
@@ -49,12 +42,10 @@ void printList(struct Node* head) {
 int main() {
     struct Node* head = NULL;
 
-    // Insert nodes at the end of the circular linked list
     insertAtEnd(&head, 1);
     insertAtEnd(&head, 2);
     insertAtEnd(&head, 3);
 
-    // Print the circular linked list
     printf("Circular Linked List: ");
     printList(head);
 
